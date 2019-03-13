e95d019b4a3608ae6c6e8161e071469277442b86
Alysson Vicuna de Oliveira <alyssontkd@gmail.com>
2019-03-13 15:19:58 -0300

    [Atualizando as senhas]
    [Atualizando as senhas]

--- classes/config.php
+++ classes/config.php
@@ -2,8 +2,8 @@
 $tiposBanco = array('mysql', 'mssql');
 
 define('DB_TYPE_SGBD', $tiposBanco[0]);
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql-pipeline');
 define('DB_NAME', 'petshop');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '12345678');
 
