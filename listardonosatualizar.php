1ee41193776788baea47800be40d316715284477
Alysson Vicuna de Oliveira <alyssontkd@gmail.com>
2019-08-22 14:22:33 -0300

    [UPDATE] colocando id no link das imagens
    [UPDATE] colocando id no link das imagens

--- listardonosatualizar.php
+++ listardonosatualizar.php
@@ -16,7 +16,7 @@ function __autoload($class_name) {
 
 	$sexo = $registro->cs_sexo == 'm'? 'Masculino' : 'Feminino';
     //echo "<tr bgcolor = 'lightsteelblue'><td><a href = 'formularioatualizardono.php?cod=$registro[0]'>" .$registro["id_dono"] . "</a></td><td>" . $registro["nm_dono"] . "</td><td>" . $registro["tx_email"] . "</td><td>" . $sexo .  "</td><td>" . $registro["nm_cidade"] . "</td></tr>";
-    echo "<tr bgcolor = 'lightsteelblue'><td><a href = 'formularioatualizardono.php?cod=$registro->id_dono'><img src='imagens/logo.jpg' width='15' heigth='30' /></a><a href = 'formularioatualizardono.php?cod=$registro->id_dono'><img src='img/logo.jpg' width='15' heigth='30' /></a></td><td>" . $registro->nm_dono . "</td><td>" . $registro->tx_email . "</td><td>" . $sexo .  "</td><td>" . $registro->nm_cidade . "</td></tr>";
+    echo "<tr bgcolor = 'lightsteelblue'><td><a href = 'formularioatualizardono.php?cod=$registro->id_dono' link='$registro->id_dono'><img src='imagens/logo.jpg' width='15' heigth='30' link='$registro->id_dono' /></a><a href = 'formularioatualizardono.php?cod=$registro->id_dono'><img src='img/logo.jpg' width='15' heigth='30' /></a></td><td>" . $registro->nm_dono . "</td><td>" . $registro->tx_email . "</td><td>" . $sexo .  "</td><td>" . $registro->nm_cidade . "</td></tr>";
    }
   echo"</table></center>";
 	echo "<hr>";
